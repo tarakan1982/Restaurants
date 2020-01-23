b3ccb018fe0ee7e594aaf3e4ef1690012e94b388
Dmitriy Borisov <dm.borisov1982@gmail.com>
2020-01-23 12:12:18 +0300

    Кастомизировал представление на главном экране. Теперь картинки не сопрекосаются, добавил кроме названия еще два лейбла

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Dmitriy Borisov on 23.01.2020.
+//  Copyright © 2020 Dmitriy Borisov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
